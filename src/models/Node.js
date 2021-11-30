d5440ce9ed86ab42d8d0b54f0bdedd3d219eb641
paneedovanie <paneedovanie@yahoo.com>
2021-11-30 15:48:01 +0800

    Fix ref

--- src/models/Node.js
+++ src/models/Node.js
@@ -4,7 +4,6 @@ const
 
 module.exports = class {
   constructor({ host = '', port = 5000, type = null }) {
-    this.ref = ref
     this.type = type
     this.model = new WebSocket(`ws://${host}:${port}`)
 
