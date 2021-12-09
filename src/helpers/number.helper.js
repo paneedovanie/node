e02a71ee1a81402bd4857aa2190a1af226b80b01
paneedovanie <paneedovanie@yahoo.com>
2021-12-09 12:48:53 +0800

    Update

--- src/helpers/number.helper.js
+++ src/helpers/number.helper.js
@@ -2,5 +2,5 @@ module.exports.precisionRoundMod = function (number, precision) {
   // var factor = Math.pow(10, precision);
   // var n = precision < 0 ? number : 0.01 / factor + number;
   // return Math.round(n * factor) / factor;
-  return number
+  return Math.round((number + Number.EPSILON) * 10000) / 10000
 }
